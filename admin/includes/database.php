18aff9910b96813999c014b35579e487840d0735
theRingleman <sam.ringleman@gmail.com>
2016-11-14 14:10:02 -0700

    rework some of the code to be more oo in the database class and add the user file

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -9,26 +9,32 @@
     }
 
     public function open_db_connection(){
-      $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-      if (mysqli_connect_errno()) {
-        die("Database connection failed badly" . mysqli_error());
+      $this->connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+      if ($this->connection->connect_errno) {
+        die("Database connection failed badly" . $this->connection->connect_error);
       }
     }
 
     public function query($sql)
     {
-      $result = mysqli_query($this->connection, $sql);
+      $result = $this->connection->query($sql);
+      $this->confirm_query($result);
       return $result;
     }
 
     private function confirm_query($result){
       if (!$result) {
-        die("Query failed");
+        die("Query failed" . $this->connection->error);
       }
     }
 
     public function escape_string($string){
-      mysqli_real_escape_string($this->connection, $string);
+      return $escaped_string = $this->connection->real_escape_string($string);
+    }
+
+    public function the_insert_id()
+    {
+      return $this->connection->insert_id;
     }
   }
 
