da00184004396b2366edfd19db5a37142aba21ab
theRingleman <sam.ringleman@gmail.com>
2016-11-14 11:33:05 -0700

    add the comments upload and users page

---
+++ admin/users.php
@@ -0,0 +1,39 @@
+<?php include("includes/header.php"); ?>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+
+    <?php include('includes/top_nav.php'); ?>
+    <?php include('includes/sidebar_nav.php') ?>
+
+    <!-- /.navbar-collapse -->
+  </nav>
+
+  <div id="page-wrapper">
+    <div class="container-fluid">
+
+      <!-- Page Heading -->
+      <div class="row">
+        <div class="col-lg-12">
+          <h1 class="page-header">
+            USERS
+            <small>Subheading</small>
+          </h1>
+          <ol class="breadcrumb">
+            <li>
+              <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
+            </li>
+            <li class="active">
+              <i class="fa fa-file"></i> Blank Page
+            </li>
+          </ol>
+        </div>
+      </div>
+        <!-- /.row -->
+
+    </div>
+    <!-- /.container-fluid -->
+  </div>
+  <!-- /#page-wrapper -->
+
+<?php include("includes/footer.php"); ?>
