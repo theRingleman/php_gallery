1f8f4b483197e6ffb40bfc8db1f1779d12c5f72c
theRingleman <sam.ringleman@gmail.com>
2016-11-15 15:48:01 -0700

    add check login function to session

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -8,6 +8,16 @@
       session_start();
     }
 
+    private function check_the_login(){
+      if(isset($_SESSION['user_id'])){
+        $this->user_id = $_SESSION['user_id'];
+        $this->signed_in = true;
+      }else {
+        unset($this->user_id);
+        $this->signed_in = false;
+      }
+    }
+
   }
 
   $session = new Session();
