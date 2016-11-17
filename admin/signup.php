cae00226c93d9d9846be79dda0789b8d8e8bba08
theRingleman <sam.ringleman@gmail.com>
2016-11-17 14:15:42 -0700

    add the signup page and functionality

---
+++ admin/signup.php
@@ -0,0 +1,64 @@
+<?php
+  require_once('includes/header.php');
+
+  $username = "";
+  $password = "";
+  $first_name = "";
+  $last_name = "";
+  $message = "";
+
+  if ($session->is_signed_in()) {
+    redirect("index.php");
+  }
+
+  if (isset($_POST['submit'])) {
+    $username = ($_POST["username"]);
+    $password = ($_POST['password']);
+    $first_name = ($_POST['first_name']);
+    $last_name = ($_POST['last_name']);
+
+    $user = new User;
+    $user->username = $username;
+    $user->password = $password;
+    $user->first_name = $first_name;
+    $user->last_name = $last_name;
+
+    if($user->create()){
+      $session->login($user);
+      redirect('index.php');
+    }else{
+      $message = "You fucked something up...";
+    }
+  }
+
+?>
+
+<div class="col-md-4 col-md-offset-3">
+	<h4 class="bg-danger"><?php if (isset($message)) {echo $message;}; ?></h4>
+
+	<form id="login-id" action="" method="post">
+		<div class="form-group">
+			<label for="username">Username</label>
+			<input type="text" class="form-control" name="username" value="<?php echo htmlentities($username); ?>" >
+		</div>
+
+		<div class="form-group">
+			<label for="password">Password</label>
+			<input type="password" class="form-control" name="password" value="<?php echo htmlentities($password); ?>">
+		</div>
+
+    <div class="form-group">
+			<label for="username">First Name</label>
+			<input type="text" class="form-control" name="first_name" value="<?php echo htmlentities($username); ?>" >
+		</div>
+
+    <div class="form-group">
+			<label for="username">Last Name</label>
+			<input type="text" class="form-control" name="last_name" value="<?php echo htmlentities($username); ?>" >
+		</div>
+
+		<div class="form-group">
+		<input type="submit" name="submit" value="Submit" class="btn btn-primary">
+		</div>
+	</form>
+</div>
